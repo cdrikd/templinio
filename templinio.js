93867e3afa9ec325d87bf56a6475b1b1099d2582
cdriko <cdriko@gmail.com>
2015-03-23 00:24:13 +0100

    ajout de l'effacement

--- templinio.js
+++ templinio.js
@@ -1,13 +1,17 @@
 // Templinio : Timeline using d3js
 // It's only a proof of concept
+//
+
 // Size of the timeline
 var w = 800;
-var h = 300;
+var h = 150;
 
 var paddingData = 5; // Padding entre les différentes dates et la hauteur
 var sizeLine = 20; // Taille en pixel des rond et rectancles ainsi que du texte
+
 // Nombre de lignes de dates possibles . Hauteur - axe - (padding haut et bas)
 var nbLine = Math.floor((h - 30 - (2 * paddingData)) / (sizeLine + paddingData));
+
 // Rayon des cercles pours les dates
 var circleRadius = 5;
 
@@ -42,12 +46,12 @@ var customTimeFormat = myFormatters.timeFormat.multi([
 ]);
 
 // Url for 30 random data http://beta.json-generator.com/Mu0JFF9
-// http://beta.json-generator.com/api/json/get/OOOGEOD   LQlLJHS AaTWszy
+// http://beta.json-generator.com/api/json/get/OOOGEOD   LQlLJHS AaTWszy DUJCqzc
 d3.json("http://beta.json-generator.com/api/json/get/OOOGEOD", function(json) {
     dataset = json;
     // On trie les dates
     dataset.sort(function(a,b){
-      return new Date(a.startDate) - new Date(b.startDate);
+        return new Date(a.startDate) - new Date(b.startDate);
     });
     generateTimeline();
 });
@@ -58,14 +62,14 @@ function generateTimeline() {
         .domain([
             new Date(d3.min(dataset, function(d) { return d.startDate; })),
             new Date(d3.max(dataset, function(d) { return d.startDate; }))
-            ])
+        ])
         .range([0,w]);
 
     var xAxis = d3.svg.axis()
-      .scale(tScale)
-      .orient("bottom")
-      .tickFormat(customTimeFormat)
-      .ticks(6);
+        .scale(tScale)
+        .orient("bottom")
+        .tickFormat(customTimeFormat)
+        .ticks(6);
 
     var svg = d3.select("#templinio-example1")
         .append("svg")
@@ -101,9 +105,9 @@ function generateTimeline() {
             }
         });
     var dates_events = newsvg.selectAll(".tdate").append("circle")
-            .attr("transform", "translate(" + circleRadius + "," + (sizeLine / 2) + ")")
-            .attr("fill","#2196F3")
-            .attr("r",circleRadius);
+        .attr("transform", "translate(" + circleRadius + "," + (sizeLine / 2) + ")")
+        .attr("fill","#2196F3")
+        .attr("r",circleRadius);
 
     // Affichage des textes des dates
     var txt_dates_events = newsvg.selectAll(".tdate").append("text")
@@ -112,7 +116,7 @@ function generateTimeline() {
         .attr("class", "values")
         .attr("text-anchor", "left")
         .attr("alignment-baseline","middle")
-        .attr("fill", "#1565C0");
+        .attr("fill", "#424242");
 
     var periods_events = newsvg.selectAll(".tperiod").append("rect")
         .attr("class","objperiod")
@@ -134,32 +138,37 @@ function generateTimeline() {
         .attr("alignment-baseline","middle")
         .attr("fill", "#6A1B9A");
 
+    // Historise le zoom pour voir si on est en scroll ou zoom. On met a zero au début pour
+    // pouvoir exécute le repositionnement vertical
+    var lastZoom = 0;
+
     zoomed();
 
     // Vérifie si les coordonnées de l'event rentre en collication avec un event déjà dessiné
     function checkOverlap(positionnedEvent, coordEvent) {
         var overlapFound = false;
         positionnedEvent.forEach(function(elt, index, array) {
-                    // Test Overlap
-          if ((Math.max(coordEvent.minX,elt.minX) < Math.min(coordEvent.maxX,elt.maxX)) && (Math.max(coordEvent.minY,elt.minY) < Math.min(coordEvent.maxY,elt.maxY))) {
-              overlapFound = true;
-          }
+            // Test Overlap
+            if ((Math.max(coordEvent.minX,elt.minX-paddingData) < Math.min(coordEvent.maxX,elt.maxX+paddingData)) && (Math.max(coordEvent.minY,elt.minY-paddingData) < Math.min(coordEvent.maxY,elt.maxY+paddingData))) {
+                overlapFound = true;
+            }
         });
         return overlapFound
     }
 
     function zoomed(d) {
-        console.log(zoom.translate());
+        console.log("debut");
         svg.select(".x.axis").call(xAxis);
         //Pour les cercle on eleve le rayon afin que le centre du cercle soit pile sur la date
         newsvg.selectAll("g.tevent.tdate").attr("transform", function(d) {
-          var tr = d3.transform(d3.select(this).attr("transform")).translate;
-          return "translate(" + (tScale(new Date(d.startDate)) - circleRadius) + "," + tr[1] + ")";
+            var tr = d3.transform(d3.select(this).attr("transform")).translate;
+            return "translate(" + (tScale(new Date(d.startDate)) - circleRadius) + "," + tr[1] + ")";
         });
         newsvg.selectAll("g.tevent.tperiod").attr("transform", function(d) {
-          var tr = d3.transform(d3.select(this).attr("transform")).translate;
-          return "translate(" + tScale(new Date(d.startDate)) + "," + tr[1] + ")";
+            var tr = d3.transform(d3.select(this).attr("transform")).translate;
+            return "translate(" + tScale(new Date(d.startDate)) + "," + tr[1] + ")";
         });
+
         // On élargie / réduit les périodes en fonction du zoom
         periods_events.attr("width", function(d) {
             return (tScale(new Date(d.endDate)) - tScale(new Date(d.startDate)));
@@ -174,48 +183,103 @@ function generateTimeline() {
         // Positionnement sur l'axe Y pour gérer les chevauchements
         // Cela suppose que les données ait été triées et donc que les balises g soient dans
         // l'ordre chronologique
-        events.each(function(d,i) {
+        if (zoom.scale() != lastZoom) {
+            events.each(function(d,i) {
+                // On calule le rectangle occupé par l'élément
+                var tr = d3.transform(d3.select(this).attr("transform")).translate;
+                var rectHeight = this.getBBox().height;
+                var coordEvent = {
+                    minX : tr[0],
+                    maxX : this.getBBox().width + tr[0],
+                    minY : tr[1],
+                    maxY : rectHeight + tr[1]
+                };
+                if (d3.select(this).classed("eventReduce")) {
+                    // Si l'évènement est caché on lui remet des coordonnées standard pour
+                    // l'évaluer
+                    coordEvent.minY = 0;
+                    coordEvent.maxY = rectHeight;
+                }
 
-            // On calule le rectangle occupé par l'élément
-            var tr = d3.transform(d3.select(this).attr("transform")).translate;
-            var rectHeight = this.getBBox().height;
-            var coordEvent = {
-              minX : tr[0],
-              maxX : this.getBBox().width + tr[0],
-              minY : tr[1],
-              maxY : rectHeight + tr[1]
-            };
-            // Si il y a une collision on essai de le repositionner au plus haut
-            if (checkOverlap(parsed,coordEvent)) {
-                coordEvent.minY = 0;
-                coordEvent.maxY = rectHeight;
-                var collisionCorrected = false;
-                while(coordEvent.maxY <= h-30) {
-                    if (! checkOverlap(parsed,coordEvent)) {
-                        // Plus de collision, on sort
-                        collisionCorrected = true;
-                        break;
-                    } else {
-                        coordEvent.minY = coordEvent.minY + 10;
-                        coordEvent.maxY = coordEvent.minY + rectHeight;
+                // Si il y a une collision on essai de le repositionner au plus haut
+                var minimizeEvent = false;
+                var maximizeEvent = false;
+                if (checkOverlap(parsed,coordEvent)) {
+                    coordEvent.minY = 0;
+                    coordEvent.maxY = rectHeight;
+                    var collisionCorrected = false;
+                    while(coordEvent.maxY <= (h - 30 - paddingData)) {
+                        if (! checkOverlap(parsed,coordEvent)) {
+                            // Plus de collision, on sort
+                            collisionCorrected = true;
+                            break;
+                        } else {
+                            coordEvent.minY = coordEvent.minY + 5;
+                            coordEvent.maxY = coordEvent.minY + rectHeight;
+                        }
                     }
+                    // Si la collision n'a pu être corrigé, on possitionne un symbole sur
+                    // l'axe x pour montrer qu'il y a d'autres valeurs et on rend invisible
+                    // la pastille et le texte
+                    if (! collisionCorrected) {
+                        coordEvent.minY = h-30;
+                        coordEvent.maxY = coordEvent.minY + rectHeight;
+                        minimizeEvent = true;
+                    } else {
+                        if (d3.select(this).classed("eventReduce")) {
+                            maximizeEvent = true;
+                        }
+                        parsed.push(coordEvent);
+                    }
+                } else {
+                    //Pas d'overlap
+                    if (d3.select(this).classed("eventReduce")) {
+                        maximizeEvent = true;
+                    }
+                    parsed.push(coordEvent);
                 }
-                // Si la collision n'a pu être corrigé, on positionne tout en haut
-                if (! collisionCorrected) {
-                    coordEvent.minY = 50;
-                    coordEvent.maxY = coordEvent.minY + rectHeight;
+                // Si on doit minimiser l'évènement
+                if (minimizeEvent) {
+                    d3.select(this).selectAll(".tempZoom").remove();
+                    //console.log(d.title + " minimize");
+                    d3.selectAll(this.childNodes).attr("opacity","0");
+                    d3.select(this).attr("transform", function () {
+                            var transform = d3.transform(d3.select(this).attr("transform"));
+                            return "translate("+transform.translate[0]+", " + coordEvent.minY + ")";
+                        })
+                        .classed("eventReduce",true)
+                        .append("circle")
+                        .attr("class","tempZoom")
+                        .attr("fill",function(d) {
+                            if(d.hasOwnProperty("endDate") && (0 !== d.endDate.length)) {
+                                // C'est une période si il a une date de fin non vide
+                                return "#9C27B0";
+                            } else {
+                                return "#2196F3";
+                            }
+                        })
+                        .attr("r",3);
+                } else if (maximizeEvent) {
+                    d3.select(this).selectAll(".tempZoom").remove();
+                    d3.select(this)
+                        .classed("eventReduce",false)
+                        .attr("transform", function () {
+                            var transform = d3.transform(d3.select(this).attr("transform"));
+                            return "translate("+transform.translate[0]+", " + coordEvent.minY + ")";
+                        })
+                        .attr("opacity","1");
+                    d3.selectAll(this.childNodes).attr("opacity","1");
+                } else {
+                    // On positionne correctement l'évènement
+                    d3.select(this).attr("transform", function () {
+                        var transform = d3.transform(d3.select(this).attr("transform"));
+                        return "translate("+transform.translate[0]+", " + coordEvent.minY + ")";
+                    });
                 }
-            }
-
-            parsed.push(coordEvent);
-            // On positionne correctement l'évènement
-            d3.select(this).attr("transform", function () {
-                 var transform = d3.transform(d3.select(this)
-                      .attr("transform"));
-                 return "translate("+transform.translate[0]+", " + coordEvent.minY + ")";
             });
-        });
+        }
+        lastZoom = zoom.scale();
+        console.log("fin");
     }
 }
-
 
